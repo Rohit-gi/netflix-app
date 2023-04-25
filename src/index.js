6319c67118be3b82a04ee7d0348ffd830854d73b
Rohit-gi <i.rohit003@gmail.com>
2023-04-25 18:59:08 +0530

    added comit

--- src/index.js
+++ src/index.js
@@ -1,27 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import Card from "./Cards"
+import "./index.css"
+
 
 ReactDOM.render(
-  <>
-    <div className="cards">
-      <div className="card">
-        <img
-          src="https://wallpapercave.com/wp/wp4056410.jpg"
-          alt="myPic"
-          className="card__img"
-        />
-        <div className="card__info">
-          <span className="card__category">A Netflix Original Series</span>
-          <h3 className="card__title"> DARK </h3>
-          <a
-            href="https://www.netflix.com/in/title/80990668?source=35"
-            target="_blank"
-          >
-            <button> Watch Now</button>
-          </a>
-        </div>
-      </div>
-    </div>
-  </>,
+<>
+<Card imgsrc="https://wallpapercave.com/wp/wp4056410.jpg"
+  title = "A Netflix Original Series"
+  sname = "DARK"
+  link = "https://www.netflix.com/in/title/80990668?source=35"
+/>
+<Card/>
+<Card/>
+</>
+,
   document.getElementById("root")
 );
