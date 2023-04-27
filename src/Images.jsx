e95b8386ecea4a00cccfe24ecb4ac2e7c67b2f02
Rohit-gi <i.rohit003@gmail.com>
2023-04-27 19:17:07 +0530

    Image-commit

--- src/Images.jsx
+++ src/Images.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Images = (props) => {
   return (
     <>
-      <img src={props.imgsrc} alt="myPic" className="card__img"/>;
+      <img src={props.imgsrc} alt="myPic" className="card__img"/>
     </>
   );
 };
