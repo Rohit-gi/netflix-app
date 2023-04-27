c861cb0e0856c2eeb7021cd268e5abf0ea26fa21
Mohit <mokumar@forrester.com>
2023-04-27 06:45:29 +0530

    night commit

---
+++ src/Sdata.jsx
@@ -0,0 +1,33 @@
+const Sdata = [
+  {
+    sname: "DARK",
+    imgsrc: "https: //wallpapercave.com/wp/wp4056410.jpg",
+    title: "A Netflix Original Series",
+    link: "https: //www.netflix.com/in/title/80100172",
+  },
+
+  {
+    sname: "EXTRACURRICULAR",
+    imgsrc: "https: //wallpapercave.com/wp/wp4056410.jpg",
+    title: "A Netflix Original Series",
+    link: "https: //www.netflix.com/in/title/80990668",
+  },
+  {
+    sname: "STRANGER THINGS",
+    imgsrc: "https: //wallpapercave.com/wp/wp1917154.jpg",
+    title: "A Netflix Original Series",
+    link: "https: //www.netflix.com/in/title/80990668?source: 35",
+  },
+  {
+    sname: "STRANGER THINGS",
+    imgsrc: "https: //wallpapercave.com/wp/wp1917154.jpg",
+    title: "A Netflix Original Series",
+    link: "https: //www.netflix.com/in/title/80990668?source: 35",
+  },
+  {
+    sname: "STRANGER THINGS",
+    imgsrc: "https: //wallpapercave.com/wp/wp1917154.jpg",
+    title: "A Netflix Original Series",
+    link: "https: //www.netflix.com/in/title/80990668?source: 35",
+  },
+];
--- src/index.js
+++ src/index.js
@@ -1,19 +1,32 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import Card from "./Cards"
-import "./index.css"
+import Card from "./Cards";
+import "./index.css";
+import Sdata from "./Sdata";
 
+console.log(Sdata[0]);
 
 ReactDOM.render(
-<>
-<Card imgsrc="https://wallpapercave.com/wp/wp4056410.jpg"
-  title = "A Netflix Original Series"
-  sname = "DARK"
-  link = "https://www.netflix.com/in/title/80990668?source=35"
-/>
-<Card/>
-<Card/>
-</>
-,
+  <>
+    <h1 className="heading_style">List of Top 5 Netflix Series in 2020</h1>
+    <Card
+      imgsrc="https://wallpapercave.com/wp/wp4056410.jpg"
+      title="A Netflix Original Series"
+      sname="DARK"
+      link="https://www.netflix.com/in/title/80100172"
+    />
+    <Card
+      imgsrc="https://wallpapercave.com/wp/wp4056410.jpg"
+      title="A Netflix Original Series"
+      sname="EXTRACURRICULAR"
+      link="https://www.netflix.com/in/title/80990668"
+    />
+    <Card
+      imgsrc="https://wallpapercave.com/wp/wp1917154.jpg"
+      title="A Netflix Original Series"
+      sname="STRANGER THINGS"
+      link="https://www.netflix.com/in/title/80990668?source=35"
+    />
+  </>,
   document.getElementById("root")
 );
