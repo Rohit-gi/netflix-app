3d9a1e63c1569740f0b1518becf2a7a8434158bb
Rohit-gi <i.rohit003@gmail.com>
2023-04-27 11:18:19 +0530

    edits

--- src/Sdata.jsx
+++ src/Sdata.jsx
@@ -3,37 +3,37 @@ const Sdata = [
     sname: "DARK",
     imgsrc: "https://wallpapercave.com/wp/wp4056407.jpg",
     title: "A Netflix Original Series",
-    link: "https: //www.netflix.com/in/title/80100172",
+    link: "https://www.netflix.com/in/title/80100172",
   },
   {
     sname: "SHADOW & BONE",
     imgsrc: "https://wallpapercave.com/wp/wp8781052.jpg",
     title: "A Netflix Original Series",
-    link: "https: //www.netflix.com/in/title/80990668",
+    link: "https://www.netflix.com/in/title/80990668",
   },
   {
     sname: "STRANGER THINGS",
     imgsrc: "https://wallpapercave.com/wp/wp11178050.png",
     title: "A Netflix Original Series",
-    link: "https: //www.netflix.com/in/title/80990668",
+    link: "https://www.netflix.com/in/title/80990668",
   },
   {
     sname: "VIKINGS",
     imgsrc: "https://wallpapercave.com/wp/wp11918549.jpg",
     title: "A Netflix Original Series",
-    link: "https: //www.netflix.com/in/title/80990668",
+    link: "https://www.netflix.com/in/title/80990668",
   },
   {
     sname: "THE WITCHER",
     imgsrc: "https://wallpapercave.com/wp/wp10426828.jpg",
     title: "A Netflix Original Series",
-    link: "https: //www.netflix.com/in/title/80990668",
+    link: "https://www.netflix.com/in/title/80990668",
   },
   {
     sname: "THE MANDALORIAN",
     imgsrc: "https://wallpapercave.com/dwp1x/wp9366113.jpg",
     title: "A Netflix Original Series",
-    link: "https: //www.netflix.com/in/title/80990668",
+    link: "https://www.starwars.com/series/the-mandalorian",
   },
 ];
 
